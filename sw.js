57901020ef2aec5bd4a3a55f89b6ca5cb024e15a
BudiSuryo <wmbudisp@gmail.com>
2025-08-02 14:56:38 +0700

    lp app sheet bpr v.1

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('bpr-kreo-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/logo1.png',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
